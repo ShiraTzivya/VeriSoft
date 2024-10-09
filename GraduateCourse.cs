aa2308be9d143702d1331f8a7b861424eb17e482
Shira Tzivya <fraiman2020@gmail.com>
2024-10-09 16:38:10 +0300

    Courses

---
+++ GraduateCourse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeriSoft
+{
+    internal class GraduateCourse : UndergraduateCourse
+    {
+        #region Attributes
+        private string researchFocus;
+        #endregion
+
+        #region Methods
+        public override string getCourseName() { return this.getCourseName(); }
+        public override string getCourseCode() { return this.getCourseCode(); }
+        public override void addParticipant(Person person) { }
+        public override Person[] getParticipants() { return default(Person[]); }
+        #endregion
+    }
+}
---
+++ UndergraduateCourse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeriSoft
+{
+    internal class UndergraduateCourse : Course
+    {
+        #region Attributes
+        private string courseName;
+        private string courseCode;
+        #endregion
+
+        #region Methods
+        public virtual string getCourseName() {  return courseName; }
+        public virtual string getCourseCode() {  return courseCode; }
+        public virtual void  addParticipant(Person person) { }
+        public virtual Person[] getParticipants() { return default(Person[]); }
+        #endregion
+
+    }
+}
