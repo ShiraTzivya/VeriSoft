931bbefd46b4a73dcdaba6aa950840607b0bd8d4
Shira Tzivya <fraiman2020@gmail.com>
2024-10-09 15:17:50 +0300

    person

--- Person.cs
+++ Person.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace VeriSoft
 {
-    internal class Person
+    internal abstract class Person
     {
+        #region Field
+        private string name;
+        private int age;
+        protected string address;
+        #endregion
+
+        #region Methods
+        public string getName() {  return name; }
+        public int getAge() { return age; }
+        public string getAddress() { return address; }
+        public abstract string getRole(); //returns a String describing the role of the person
+        #endregion
     }
 }
